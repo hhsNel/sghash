7201443825ebff61233cf405d656835ab347475a
hhsNel <hihijsonel@gmail.com>
2025-05-25 16:00:40 +0200

    hashing stdin

--- hash.h
+++ hash.h
@@ -12,11 +12,28 @@ union hash {
 };
 
 union hash make_hash(char *str);
+union hash make_hash_stdin();
 
 union hash make_hash(char *str) {
 	union hash hash;
 
 	SHA512((const unsigned char *)str, strlen(str), hash.bytes);
+	return hash;
+}
+
+union hash make_hash_stdin() {
+	SHA512_CTX ctx;
+	unsigned char buff[4096];
+	size_t bytes;
+	union hash hash;
+
+	SHA512_Init(&ctx);
+	
+	while((bytes = fread(buff, 1, sizeof(buff), stdin)) > 0) {
+		SHA512_Update(&ctx, buff, bytes);
+	}
+
+	SHA512_Final(hash.bytes, &ctx);
 	return hash;
 }
 
--- main.c
+++ main.c
@@ -5,30 +5,39 @@
 #include "drawing.h"
 #include "hash.h"
 
+void out_image(union hash h, unsigned int i);
+
 int main(int argc, char **argv) {
 	unsigned int i;
-	union  hash h;
-	image img;
-	char buff[8];
+	union hash h;
 
 	if(argc == 1) {
-		printf("No arguments, exiting\n");
+		printf("No arguments, reading from stdin\n");
+		h = make_hash_stdin();
+		out_image(h, 0);
 		return 0;
 	}
 	
 	for(i = 1; i < argc; ++i) {
 		printf("Hashing %s...\n", argv[i]);
 		h = make_hash(argv[i]);
-		printf("Hashed, preparing image...\n");
-		img = init_image();
-		printf("Drawing image...\n");
-		draw_image(img, h.cmds, 64 / sizeof(brush_command));
-		printf("Saving image...\n");
-		snprintf(buff, 7, "%u.ff", i);
-		save_farbfeld(img, buff);
-		printf("Done\n");
+		out_image(h, i);
 	}
 
 	return 0;
+}
+
+void out_image(union hash h, unsigned int i) {
+	image img;
+	char buff[8];
+
+	printf("Hashed, preparing image...\n");
+	img = init_image();
+	printf("Drawing image...\n");
+	draw_image(img, h.cmds, 64 / sizeof(brush_command));
+	printf("Saving image...\n");
+	snprintf(buff, 7, "%u.ff", i);
+	save_farbfeld(img, buff);
+	printf("Done\n");
 }
 
